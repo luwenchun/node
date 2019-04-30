818f0995716f3005c7d6a63ca8e48e368ddddb91
luwenchun <luwenchun@users.noreply.github.com>
2019-04-30 13:20:23 +0800

    express构建http服务器

---
+++ 07-express-test.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  // 读取首页
+  // console.log(path.resolve('./index.html'));
+  const {url,method} = req;
+  fs.readFile(path.resolve("./index.html"), (err, data) => {
+    if (err) {
+      res.statusCode = 500; // 服务器内部错误
+      res.end("500 - Internal Server Error!");
+      return;
+    }
+    res.statusCode = 200; // 请求成功
+    res.setHeader("Content-Type", "text/html");
+    // console.log(data)
+    // if(req.headers.accept.indexOf('image/*')!==-1 && method === 'GET') {
+    //   console.log('.'+url);        
+    //   fs.createReadStream(path.resolve('.'+url)).pipe(res);
+    //   } 
+    res.end(data);
+  });
+   
+});
+app.get("/users", (req, res) => {
+  // 接口编写
+  res.statusCode = 200; // 请求成功
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify([{ name: "tom", age: 20 }]));
+});
+
+app.listen(3000);
